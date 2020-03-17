7e563d358894e80936e8733a825edf7b0abe3c71
note <note16651@hotmail.co.th>
2020-03-17 21:30:06 +0700

    [ADD]

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="x" /> 
+    {{op}}
+    <input v-model="y" />
+    <button @click="showingNum(num)" v-for="num in arrNums" :key="num">{{num}}</button>
+    <button @click="operation(op)" v-for="op in arr" :key="op">{{op}}</button>
+    <button @click="calculate(op)">Calculate</button>
+    <p>{{result}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+/*  eslint-disable*/
+data(){
+  return {
+    x:"",
+    y:"",
+    arr:['+','-','*','/'],
+    result:0,
+    arrNums:['1','2','3','4','5','6','7','8','9'],
+    First:true,
+    op:'+'
+    
+    }
+},
+methods:{
+  operation(op){
+    
+  if(op=='+'){
+      this.op='+'
+      this.First=false
+  }else if(op=='-'){
+      this.op='-'
+     this.First=false
+  }else if(op=='*'){
+    this.op='*'
+    this.First=false
+  }else if(op=='/'){
+    this.op='/'
+    this.First=false
+     }
+    },
+    showingNum(num){
+      this.x += ""
+      this.y += ""
+      if(this.First){
+        this.x+=num
+      }else{
+        this.y+=num    
+    }
+  },
+  calculate(op){
+    this.x=parseInt(this.x)
+    this.y=parseInt(this.y)
+    if(this.op=='+'){
+      this.result=this.x+this.y
+  }else if(this.op=='-'){
+    this.result=this.x-this.y
+    }else if(this.op=='*'){
+      this.result=this.x*this.y
+      }else if(this.op=='/'){
+        this.result=this.x/this.y
+        }
+}
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
